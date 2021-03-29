40f7ee95f3265217c2a2d8fdf49c7833a742754e
Adetula <olaadetula@gmail.com>
2021-03-29 10:54:40 -0400

    middleware

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  config.session_store :cookie_store
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+  config.middleware.use Rack::MethodOverride
 end
