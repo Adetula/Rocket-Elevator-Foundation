d8ce6075258cc88ac5cde8d7e5d66f465fc23df0
Adetula <olaadetula@gmail.com>
2021-03-29 10:31:22 -0400

    caps

--- app/models/warehouse_record.rb
+++ app/models/warehouse_record.rb
@@ -1,4 +1,5 @@
 class WarehouseRecord < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection :"warehouse_#{Rails.env}"
+  establish_connection :"warehouse_development"
+  #for local test, use establish_connection :"warehouse_#{Rails.env}"
 end
