c5baaf3ba7fccda689b53fdc612a18b868c94081
Adetula <olaadetula@gmail.com>
2021-04-04 08:41:29 -0400

    live ready

--- app/models/warehouse_record.rb
+++ app/models/warehouse_record.rb
@@ -2,6 +2,6 @@ class WarehouseRecord < ActiveRecord::Base
   self.abstract_class = true
   # for live test, use establish_connection :"warehouse_development"
   # establish_connection :"warehouse_#{Rails.env}"
-  establish_connection :"warehouse_development
-  #for local test, use establish_connection :"warehouse_#{Rails.env}"
+  establish_connection :"warehouse_development"
+  # for local test, use establish_connection :"warehouse_#{Rails.env}"
 end
